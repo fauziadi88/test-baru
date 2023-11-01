5bc52f8d3cd02bbd9c0d65e1b9fae88c0a3c0ff0
fauziadi88 <53512000+fauziadi88@users.noreply.github.com>
2023-11-01 10:24:13 +0700

    Add files via upload

---
+++ Konfigurasi.py
@@ -0,0 +1,145 @@
+#import library yang dibutuhkan
+
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pathlib
+import os.path
+import pandas as pd
+import datetime
+import altair as alt
+import numpy as np
+from sklearn import tree
+from csv import writer
+from web_functions import load_data
+from web_functions import predict_KNN
+from web_functions import predict_DT
+from web_functions import predict_LR
+from web_functions import predict_RF
+from web_functions import train_model_KNN
+from web_functions import train_model_DT
+from web_functions import train_model_LR
+from web_functions import train_model_RF
+
+# memanggil dataset
+df,x,y = load_data()
+
+#Menu Bar
+
+
+# Judul dari tab kanan
+st.title("Prediksi Data IHSG")
+
+# Menampilkan 5 Data Teratas dan Terbawah
+first_row = df.head(5)
+separator = pd.DataFrame({'Date': ['...'],'Open' : ['...'], 'Close':['...']})
+last_row = df.tail(5)
+result = pd.concat([first_row,separator,last_row])
+st.header("Tabel dataset (5 Data Teratas & Terbawah)")
+st.table(result)
+
+
+#membuat sidebar title
+st.sidebar.title("Konfigurasi")
+st.sidebar.write('Pilih Algoritma yang akan kamu pakai:')
+knn = st.sidebar.checkbox('KNN')
+lr = st.sidebar.checkbox('Linear Regression')
+rf = st.sidebar.checkbox('Random Forest')
+dt = st.sidebar.checkbox('Decision Tree')
+
+tanggal = str(df.Date.tail(1))
+tanggal = tanggal.split()
+tanggal = tanggal[1].split("/")
+
+hari = int(tanggal[0])
+bulan = int(tanggal[1])
+tahun = int('20'+tanggal[2])
+date = st.sidebar.date_input(
+    "Tanggal Akhir Prediksi",
+    datetime.date(tahun, bulan, hari)+ datetime.timedelta(days=1),
+    min_value=datetime.date(tahun, bulan, hari)
+)
+date_def = datetime.date(tahun, bulan, hari)
+day = (date-date_def).days
+
+features = [y.iloc[-1]]
+#Tombol Prediksi
+button = st.sidebar.button("Prediksi")
+tab1, tab2, tab3, tab4 = st.tabs(["KNN", "Linier Regresion","Random Forest","Decision Tree"])
+if button:
+    if knn:
+        with tab1:
+            predictionknn = predict_KNN(x,y,features,day,tanggal)
+            st.subheader("Tabel Hasil KNN")
+            st.table(predictionknn)
+    else:
+        with tab1:
+            st.caption("Untuk menampilkan hasil prediksi menggunakan algoritma KNN silahkan beri tanda check algoritma KNN pada menu konfigurasi")
+    if lr:
+        with tab2:
+            predictionlr = predict_LR(x,y,features,day,tanggal)
+            st.subheader("Tabel Hasil Linier Regresion")
+            st.table(predictionlr)
+    else:
+        with tab2:
+            st.caption("Untuk menampilkan hasil prediksi menggunakan algoritma Linier Regresion silahkan beri tanda check algoritma Linier Regresion pada menu konfigurasi")
+    if rf:
+        with tab3:
+            predictionrf = predict_RF(x,y,features,day,tanggal)
+            st.subheader("Tabel Hasil Random Forest")
+            st.table(predictionrf)
+    else:
+        with tab3:
+            st.caption("Untuk menampilkan hasil prediksi menggunakan algoritma Random Forest silahkan beri tanda check algoritma Random Forest pada menu konfigurasi")
+    if dt:
+        with tab4:
+            predictiondt = predict_DT(x,y,features,day,tanggal)
+            st.subheader("Tabel Hasil Decision Tree")
+            st.table(predictiondt)
+    else:
+        with tab4:
+            st.caption("Untuk menampilkan hasil prediksi menggunakan algoritma Decision Tree silahkan beri tanda check pada menu konfigurasi")
+
+if button:
+    st.subheader("Grafik Pembukaan Harga")
+    st.caption("Untuk menampilkan grafik prediksi pembukaan harga silahkan beri tanda check pada algoritma KNN, Linier Regresion, Random Forest dan Decision Tree pada menu konfigurasi")
+    if dt and rf and lr and knn:
+        df['Date'] = pd.to_datetime(df.Date, format='%d/%m/%y').dt.strftime('%Y-%m-%d')
+        df['Type'] = "Data Historis"
+        predictiondt["Type"] = "Decision Tree"
+        predictiondt = [df,predictiondt]
+        predictiondt = pd.concat(predictiondt)
+        predictiondt['Date'] = pd.to_datetime(predictiondt.Date, format='%Y-%m-%d')
+        predictiondt = predictiondt.sort_values(by=['Date'])
+
+        df['Date'] = df.Date
+        predictionrf["Type"] = "Random Forest"
+        predictionrf = [df,predictionrf]
+        predictionrf = pd.concat(predictionrf)
+        predictionrf['Date'] = pd.to_datetime(predictionrf.Date, format='%Y-%m-%d')
+        predictionrf = predictionrf.sort_values(by=['Date'])
+
+        df['Date'] = df.Date
+        predictionlr["Type"] = "Linier Regresion"
+        predictionlr = [df,predictionlr]
+        predictionlr = pd.concat(predictionlr)
+        predictionlr['Date'] = pd.to_datetime(predictionlr.Date, format='%Y-%m-%d')
+        predictionlr = predictionlr.sort_values(by=['Date'])
+
+        df['Date'] = df.Date
+        predictionknn["Type"] = "KNN"
+        predictionknn = [df,predictionknn]
+        predictionknn = pd.concat(predictionknn)
+        predictionknn['Date'] = pd.to_datetime(predictionknn.Date, format='%Y-%m-%d')
+        predictionknn = predictionknn.sort_values(by=['Date'])
+
+        dataclose = pd.concat([predictionrf, predictiondt, predictionlr, predictionknn])
+        c = (
+        alt.Chart(dataclose)
+        .mark_trail()
+        .encode(x="Date:T", y="Open", size="Type", color="Type", tooltip=["Date", "Open", "Type"])
+        .interactive()
+        )
+
+        st.altair_chart(c, use_container_width=True)
+
---
+++ web_functions.py
@@ -0,0 +1,167 @@
+import numpy as np
+import pandas as pd
+import datetime
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+import streamlit as st
+
+def load_data():
+    #menambahkan dataset
+    df = pd.read_csv("dataset.csv")
+    #menghapus semua spasi dari nama kolom
+    df.columns = list(map(lambda a: a.lstrip(), df.columns))
+    
+    #mensett x
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    numeric = df.select_dtypes(include=numerics)
+
+    jumlah_column = len(numeric.axes[1])
+    
+    x = df.iloc[:, [1]]
+    y = df.iloc[:, -1]
+    return df, x,y
+
+def train_model_DT(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=49)
+    # membuat model Decision Tree
+
+    tree_model = DecisionTreeRegressor()
+
+    # melakukan pelatihan model terhadap data
+    tree_model = tree_model.fit(X_train, y_train)
+    
+    y_pred = tree_model.predict(X_test)
+
+    return tree_model
+    
+
+def predict_DT(x,y,features,day, tanggal):
+    tree_model = train_model_DT(x,y)
+    hari = int(tanggal[0])
+    bulan = int(tanggal[1])
+    tahun = int('20'+tanggal[2])
+
+    date = []
+    opened = []
+    closed = []
+    for i in range(day):
+        date.append(datetime.date(tahun, bulan, hari) + datetime.timedelta(days=i+1))
+        
+        if i == 0:
+            opened.append(features[0])
+            closed.append(tree_model.predict(np.array(features).reshape(1,-1))[0])
+        else:
+            opened.append(closed[i-1])
+            closed.append(tree_model.predict(np.array(closed[i-1]).reshape(1,-1))[0])
+    predict = pd.DataFrame({'Date': date, 'Open': opened, 'Close':closed})
+    
+    return predict
+
+    
+def train_model_KNN(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=49)
+    
+    knn = KNeighborsRegressor(n_neighbors=2)
+    
+    knn = knn.fit(X_train, y_train)
+
+    return knn
+
+def predict_KNN(x, y, features,day, tanggal):
+    knn = train_model_KNN(x, y)
+
+    hari = int(tanggal[0])
+    bulan = int(tanggal[1])
+    tahun = int('20'+tanggal[2])
+
+    date = []
+    opened = []
+    closed = []
+    for i in range(day):
+        date.append(datetime.date(tahun, bulan, hari) + datetime.timedelta(days=i+1))
+        
+        if i == 0:
+            opened.append(features[0])
+            
+            closed.append(knn.predict(np.array(features,dtype=np.float64).reshape(1,-1))[0])
+        else:
+            opened.append(closed[i-1])
+            closed.append(knn.predict(np.array(closed[i-1],dtype=np.float64).reshape(1,-1))[0])
+    predict = pd.DataFrame({'Date': date, 'Open': opened, 'Close':closed})
+    
+    return predict
+
+def train_model_LR(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=49)
+    linier_model = LinearRegression()
+    linier_model = linier_model.fit(X_train, y_train)
+
+
+
+    return linier_model
+
+def predict_LR(x, y, features, day, tanggal):
+    lr = train_model_LR(x, y)
+
+    hari = int(tanggal[0])
+    bulan = int(tanggal[1])
+    tahun = int('20'+tanggal[2])
+
+    date = []
+    opened = []
+    closed = []
+    for i in range(day):
+        date.append(datetime.date(tahun, bulan, hari) + datetime.timedelta(days=i+1))
+        
+        if i == 0:
+            opened.append(features[0])
+            closed.append(lr.predict(np.array(features,dtype=np.float64).reshape(1,-1))[0])
+        else:
+            opened.append(closed[i-1])
+            closed.append(lr.predict(np.array(closed[i-1],dtype=np.float64).reshape(1,-1))[0])
+    predict = pd.DataFrame({'Date': date, 'Open': opened, 'Close':closed})
+    return predict
+
+def train_model_RF(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=49)
+    rfr = RandomForestRegressor(n_estimators=100)
+    rfr_model = rfr.fit(X_train, y_train)
+
+
+
+    return rfr_model
+
+def predict_RF(x, y, features, day, tanggal):
+    rf = train_model_RF(x, y)
+
+    hari = int(tanggal[0])
+    bulan = int(tanggal[1])
+    tahun = int('20'+tanggal[2])
+
+    date = []
+    opened = []
+    closed = []
+    for i in range(day):
+        date.append(datetime.date(tahun, bulan, hari) + datetime.timedelta(days=i+1))
+        
+        if i == 0:
+            opened.append(features[0])
+            closed.append(rf.predict(np.array(features,dtype=np.float64).reshape(1,-1))[0])
+        else:
+            opened.append(closed[i-1])
+            closed.append(rf.predict(np.array(closed[i-1],dtype=np.float64).reshape(1,-1))[0])
+    predict = pd.DataFrame({'Date': date, 'Open': opened, 'Close':closed})
+    return predict
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
